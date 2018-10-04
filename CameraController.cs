0e269d81be97f3c47b2def7b6985f465efc7bcec
Antoine <yvan.antoine84@gmail.com>
2018-10-04 14:59:46 +0200

    First commit

---
+++ CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform perso;
+	public Vector3 offset;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (perso.position.x + 0, 0, -10);
+	}
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller; 
+	public Animator animator;
+	public Rigidbody2D rigidBody2D;
+	public float runSpeed = 40f;
+	private 
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool sprint = false;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+		if (Input.GetButtonDown ("Jump")) 
+		{
+			jump = true;
+			animator.SetBool("IsJumping", true);
+		}
+	}
+
+	void FixedUpdate () {
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+		jump = false;
+	}
+
+	public void OnLanding()
+	{
+		animator.SetBool ("IsJumping", false);
+	}
+
+}
